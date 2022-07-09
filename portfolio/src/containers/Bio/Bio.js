95bbaa79567b94f7ac6986001f1943bfc2f3b67d
Alan Paul Mir <alanpaulphotos@gmail.com>
2022-07-09 20:05:23 +0000

    Added most elements to bio

--- portfolio/src/containers/Bio/Bio.js
+++ portfolio/src/containers/Bio/Bio.js
@@ -2,7 +2,42 @@ import React from 'react';
 
 const Bio = () => {
   return (
-    <div>Bio</div>
+    <div className='bio'>
+      <div className='b-title'>
+        <h4>Acerca de</h4>
+        <h6>Cinematografo. Cineasta. Narador.</h6>
+      </div>
+
+      <div className='b-para'>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud
+          exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit
+          in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident,
+          sunt in culpa qui officia deserunt mollit anim id est
+          laborum.
+        </p>
+      </div>
+
+      <div>
+        <p>Here will go an image</p>
+      </div>
+    </div>
   );
 };
 
