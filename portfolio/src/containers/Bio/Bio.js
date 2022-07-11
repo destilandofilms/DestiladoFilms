542a04ba966915795b2caa549b84509db1b5a35e
Alan Mir <alanpaulphotos@gmail.com>
2022-07-11 15:09:05 -0500

    Added more styling

--- portfolio/src/containers/Bio/Bio.js
+++ portfolio/src/containers/Bio/Bio.js
@@ -5,38 +5,40 @@ import "./Bio.scss";
 const Bio = () => {
   return (
     <div className='bio'>
-      <div className='b-title'>
-        <h4>Acerca de</h4>
-        <h6>Cinematografo. Cineasta. Narador.</h6>
+      <div className='b-info'>
+        <div className='b-title'>
+          <h4>Acerca de</h4>
+          <h6>Cinematografo. Cineasta. Narador.</h6>
+        </div>
+
+        <div className='b-para'>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit
+            in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore
+            magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea
+            commodo consequat. Duis aute irure dolor in reprehenderit
+            in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident,
+            sunt in culpa qui officia deserunt mollit anim id est
+            laborum.
+          </p>
+        </div>
       </div>
 
-      <div className='b-para'>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit
-          in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est
-          laborum.
-        </p>
-
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud
-          exercitation ullamco laboris nisi ut aliquip ex ea
-          commodo consequat. Duis aute irure dolor in reprehenderit
-          in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident,
-          sunt in culpa qui officia deserunt mollit anim id est
-          laborum.
-        </p>
-      </div>
-
-      <div>
+      <div className='b-img'>
         <p>Here will go an image</p>
       </div>
     </div>
