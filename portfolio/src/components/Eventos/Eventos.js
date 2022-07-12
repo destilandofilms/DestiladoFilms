e7610a97ad351a5d2f699f38f38584331556473f
Alan Mir <alanpaulphotos@gmail.com>
2022-07-12 14:29:12 -0500

    Decided to switch react-youtube for ReactPlayer

--- portfolio/src/components/Eventos/Eventos.js
+++ portfolio/src/components/Eventos/Eventos.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ReactPlayer from "react-player"
 
 import "./Eventos.scss";
 
 const Eventos = () => {
   return (
     <div className='eventos'>
-      Eventos
+      <ReactPlayer
+        url="https://www.facebook.com/114394897358061/videos/1048106535814777"
+      />
     </div>
   );
 };
