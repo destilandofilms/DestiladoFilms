927d235862388c3c27ca36d65564cb1e09219f2e
Alan Mir <alanpaulphotos@gmail.com>
2022-07-14 13:48:53 -0500

    Finished Eventos container

--- portfolio/src/containers/Eventos/Eventos.js
+++ portfolio/src/containers/Eventos/Eventos.js
@@ -1,26 +1,18 @@
 import React from 'react';
 import Carousel from '../../components/Carousel/Carousel';
-// import Gallery from '../../components/Gallery/Gallery';
 
+import nightclub from '../../assests/nightclub.jpg'
 import "./Eventos.scss";
-
-
-// const images = [
-//   'https://www.facebook.com/114394897358061/videos/1048106535814777',
-//   'https://www.facebook.com/114394897358061/videos/689680548678795',
-//   'https://www.facebook.com/114394897358061/videos/954431315131648',
-//   'https://www.facebook.com/114394897358061/videos/397709095522032',
-//   'https://www.facebook.com/114394897358061/videos/300897048791208',
-// ];
-
 
 const Eventos = () => {
 
   return (
     <div className='eventos'>
-      {/* <Gallery /> */}
-
       <Carousel />
+
+      <div className='e-img'>
+        <img src={nightclub} alt='Bottom banner'/>
+      </div>
     </div>
   );
 };
