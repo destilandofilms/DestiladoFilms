7f396f36165df13f0ea9f67a626845441886837a
Alan Mir <alanpaulphotos@gmail.com>
2022-07-14 13:16:05 -0500

    Added dynamic loading screen for videos

--- portfolio/src/components/Carousel/CarouselItem.js
+++ portfolio/src/components/Carousel/CarouselItem.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactPlayer from 'react-player';
 
 const CarouselItem = ({ video, width}) => {
+    const [loading, setLoading] = useState(true);
+
   return (
     <div className='c-item' style={{ width: width}}>
         <h3>{video.title}</h3>
 
+        {loading ? <div className='loader'><div className="lds-ring"><div></div><div></div><div></div><div></div></div></div> : null}
+
         <ReactPlayer 
-            className='react-player'
+            className={`react-player`}
             url={video.url}
             controls={true}
             pip={true}
+            onReady={() => setLoading(false)}
         />
 
         <p>{video.text}</p>
