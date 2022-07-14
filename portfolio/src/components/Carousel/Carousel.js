56fa95512f18cafc461677ccbd4b36b12a52eed4
Alan Mir <alanpaulphotos@gmail.com>
2022-07-14 13:40:30 -0500

    Added more animations to carousel

--- portfolio/src/components/Carousel/Carousel.js
+++ portfolio/src/components/Carousel/Carousel.js
@@ -56,18 +56,54 @@ const Carousel = () => {
 
             <div className='indicators'>
                 <button
-                    className='left'
                     onClick={() => {
                         updateIndex(activeIndex - 1)
                     }}
-                />
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                    >
+                        <path
+                            d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z"
+                        />
+                        <path
+                            d="M13.293 7.293 8.586 12l4.707 4.707 1.414-1.414L11.414 12l3.293-3.293-1.414-1.414z"
+                        />
+                    </svg>
+                </button>
+
+                {videoProperties.map((video) => {
+                    return (
+                        <button
+                            key={video.id}
+                            className={`middle ${video.id === (activeIndex + 1) ? 'active' : ''}`}
+                            onClick={() => {
+                                updateIndex(video.id - 1);
+                            }}
+                        />
+                    )
+                })}
 
                 <button
-                    className='right'
                     onClick={() => {
                         updateIndex(activeIndex + 1)
                     }}
-                />
+                >
+                    <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                    >
+                        <path
+                            d="M12 2a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2zm0 18a8 8 0 1 1 8-8 8.009 8.009 0 0 1-8 8z"
+                        />
+                        <path
+                            d="M9.293 8.707 12.586 12l-3.293 3.293 1.414 1.414L15.414 12l-4.707-4.707-1.414 1.414z"
+                        />
+                    </svg>
+                </button>
             </div>
         </div>
     );
