e452ce35bbe3eb04ba1bd956583415fa9ea93c19
Alan Mir <alanpaulphotos@gmail.com>
2022-07-14 14:01:27 -0500

    Finished Music container

--- portfolio/src/containers/Music/Music.js
+++ portfolio/src/containers/Music/Music.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Carousel from '../../components/Carousel/Carousel';
 
+import musicvideo from '../../assests/musicvideo.jpg';
 import "./Music.scss";
 
 const Music = () => {
   return (
-    <div>Music</div>
+    <div className='music'>
+      <Carousel />
+
+      <div className='m-img'>
+        <img src={musicvideo} alt='Bottom Banner' />
+      </div>
+    </div>
   );
 };
 
