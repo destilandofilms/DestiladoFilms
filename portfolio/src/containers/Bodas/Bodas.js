5fda93de3fbecfc3cd807d9129495936745e4bd3
Alan Mir <alanpaulphotos@gmail.com>
2022-07-14 13:56:58 -0500

    Finished Bodas container

--- portfolio/src/containers/Bodas/Bodas.js
+++ portfolio/src/containers/Bodas/Bodas.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import Carousel from '../../components/Carousel/Carousel';
 
+import wedding from '../../assests/wedding.jpg';
 import "./Bodas.scss";
 
 const Bodas = () => {
   return (
-    <div>Bodas</div>
+    <div className='bodas'>
+      <Carousel />
+
+      <div className='b-img'>
+        <img src={wedding} alt='Bottom Banner'/>
+      </div>
+    </div>
   );
 };
 
