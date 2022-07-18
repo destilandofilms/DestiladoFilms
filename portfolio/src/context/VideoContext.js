646f2b6a9eb741c09078e30a6c66b71349ddebbe
Alan Mir <alanpaulphotos@gmail.com>
2022-07-18 14:50:29 -0500

    Created context for videos

---
+++ portfolio/src/context/VideoContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useReducer } from "react";
+
+import data from './VideosData.json';
+
+const initialState = {
+    videos: data
+};
+
+const VideoReducer = ( state = initialState, action ) => {
+
+};
+
+export const VideoContext = createContext();
+
+export const VideoProvider = (props) => {
+    const [state, dispatch] = useReducer(VideoReducer, initialState);
+
+    return (
+        <VideoContext.Provider value={{ videos: state.videos, dispatch }}>
+            {props.children}
+        </VideoContext.Provider>
+    )
+};
