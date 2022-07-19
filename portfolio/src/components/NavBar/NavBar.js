7eb7c1ffb95534b35298abecf925e468bc09bb8f
Alan Mir <alanpaulphotos@gmail.com>
2022-07-19 15:46:49 -0500

    Finished making everything mobile responsive

--- portfolio/src/components/NavBar/NavBar.js
+++ portfolio/src/components/NavBar/NavBar.js
@@ -6,27 +6,29 @@ import Logo from "../../assests/destilado.jpg";
 import "./NavBar.scss";
 
 const NavBar = () => {
-    const [ checked, setChecked ] = useState(false);
+    const [checked, setChecked] = useState(false);
 
     console.log(checked)
 
     return (
         <div className='menu'>
-            <label htmlFor='toggle'>&#9776;</label>
-            <input type="checkbox" id="toggle" checked={checked} onChange={(e) => setChecked(e.target.checked)} />
-
             <div className='logo'>
-                <NavLink to="/"><img src={Logo} alt='Destilado Films Logo'/></NavLink>
+                <NavLink to="/"><img src={Logo} alt='Destilado Films Logo' /></NavLink>
             </div>
 
-            <nav>
-                <NavLink to="/eventos" style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)} >Eventos</NavLink>
-                <NavLink to='/bodas' style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)} >Bodas</NavLink>
-                <NavLink to='/musica' style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)}>Musica</NavLink>
-                <NavLink to="/bio" style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)}>Bio</NavLink>
-                <NavLink to="/equipo" style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)}>Equipo</NavLink>
-                <NavLink to='/contacto' style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)}>Contacto</NavLink>
-            </nav>
+            <div className='m-nav'>
+                <label htmlFor='toggle'>&#9776;</label>
+                <input type="checkbox" id="toggle" checked={checked} onChange={() => setChecked(!checked)} />
+
+                <nav>
+                    <NavLink to="/eventos" style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)} >Eventos</NavLink>
+                    <NavLink to='/bodas' style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)} >Bodas</NavLink>
+                    <NavLink to='/musica' style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)}>Musica</NavLink>
+                    <NavLink to="/bio" style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)}>Bio</NavLink>
+                    <NavLink to="/equipo" style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)}>Equipo</NavLink>
+                    <NavLink to='/contacto' style={({ isActive }) => (isActive ? { color: '#B39F79' } : null)} onClick={() => setChecked((c) => !c)}>Contacto</NavLink>
+                </nav>
+            </div>
         </div>
     );
 };
